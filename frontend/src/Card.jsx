90bafc61a3826b81887062230b98debf314234a6
Yu-yang Chen <yca439@sfu.ca>
2024-07-08 22:54:49 -0700

    update frontend images and additional jsx file

---
+++ frontend/src/Card.jsx
@@ -0,0 +1,15 @@
+
+function Card() {
+  return(
+    <div className="card">
+      <img className="card-image" src="https://via.placeholder.com/200" alt="pic1" />
+      <h2 className="card-title">Health Tips</h2>
+      <p>Set up later</p>
+
+
+
+    </div>
+  );
+}
+
+export default Card
